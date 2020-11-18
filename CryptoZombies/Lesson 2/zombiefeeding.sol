85c2bff7cd209288a9b63c09183e2b3ded2dc4ac
Adam Harner <adamj.harner@gmail.com>
2020-11-18 10:52:13 -0500

    Final edits from lesson 2

--- CryptoZombies/Lesson 2/zombiefeeding.sol
+++ CryptoZombies/Lesson 2/zombiefeeding.sol
@@ -20,16 +20,24 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
+  //cryptokitties contract address
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   //intialize kittyContract using ckAddress from above
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
   //Will give zombie ability to feed / multiply
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  //Modified to include species and store in memory
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);  //check the one feeding is the owner
     Zombie storage myZombie = zombies[_zombieId];     //Get zombie DNA and store it to blockchain
     _targetDna = _targetDna % dnaModulus; //Last 16 digits only
     uint newDna = (myZombie.dna + _targetDna ) / 2; //set new DNA to avg of target dna and myzombie dna (use .dna for accessing struct info)
+
+    //If statement to compare hashes, use abi.encodePacked to pass to keccak since strings can't be passed directly.
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      //Replace last 2 digits of DNA with 9, e.g. if newDna is 334455, then newDna % 100 is 334400 so adding 99 gives us 334499
+      newDna = newDna - newDna % 100 + 99; 
+    }
     _createZombie("NoName", newDna);
   }
 
@@ -37,7 +45,9 @@ contract ZombieFeeding is ZombieFactory {
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
       uint kittyDna;
       (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-      feedAndMultiply(_zombieId, kittyDna);
+      
+      //modified func call to include "kitty" in parameters
+      feedAndMultiply(_zombieId, kittyDna, "kitty"); 
     }
 
 }
